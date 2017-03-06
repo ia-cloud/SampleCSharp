940fe931f9c19fb4a7a1692ed40dbcc22bbe47ae
tomoki1207 <tomoki1207@gmail.com>
2017-03-06 15:39:15 +0900

    initial commit

---
+++ IaCloudCsSample/IaCloudCsSample/Program.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) 2016 K.T.System Co., Ltd. All Rights Reserved. See License in the project root.
+
+using System;
+using System.Collections.Generic;
+using IACloudCs.Https;
+using IACloudCs.ObjectModels;
+
+namespace IaCloudCsSample
+{
+    class Program
+    {
+        // テストデータをクラウド上のCCSへPOSTするサンプルです
+        static void Main(string[] args)
+        {
+            // クライアントクラスの生成
+            using (var client = new HttpsClient(new JsonContentResolver()))
+            {
+                // ia-cloud関連の設定
+                client.FDSKey = "ia-cloud";
+                client.UserID = "ia-cloud";
+                client.BasicAuthUserID = "ia-cloud-user1";
+                client.BasicAuthPassword = "ia-cloud-passoword";
+                client.ServiceUrl = "https://test.iacloud.com/iaCloudTest/v2.0";
+
+                // 生産実績オブジェクトを作成
+                var productionResult = new IACloudObject
+                {
+                    ObjectKey = "com.ia-cloud.sample.fds.win-cs",
+                    TimeStamp = DateTime.Now,
+                    ObjectDescription = "Production Result of M001",
+                    ObjectContent = new ObjectContent
+                    {
+                        ContentType = "ProductionResult",
+                        ContentData = new List<ContentDatum>
+                        {
+                            new ContentDatum { DataName = "PlannedQuantity", DataValue = 1000 },
+                            new ContentDatum { DataName = "ProducedQuantity", DataValue = 990 },
+                            new ContentDatum { DataName = "GoodQuantity", DataValue = 988 },
+                            new ContentDatum { DataName = "ScrapQuantity", DataValue = 2 },
+                            new ContentDatum { DataName = "StartDate", DataValue = "2016-10-01T09:45+09:00" }
+                        }
+                    }
+                };
+
+                // サーバへPOST
+                client.Store(productionResult);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# ia-cloud Cs
+
+ia-cloud クライアント(FDS)用のC#ライブラリ
+機器から取得したデータを ia-cloud にて策定されたフォーマットでWebサーバへ送信します
+
+## ia-cloud について
+WIP
+
+## サンプル
+[サンプル](./IaCloudCsSample/IaCloudCsSample/Program.cs)を参考にしてください
+
+## リリースノート
++ --
+
+## お問い合わせ
++ 本ライブラリ及びサンプルに関するお問い合わせ xxxx@ktsystem.jp
++ ia-cloud に関するお問い合わせ yyyy@ia-cloud.com
+
+## ライセンス
+(c) 2016 K.T.System Co., Ltd. All rights reserved.
+ライセンスに関する条項は [LICENSE](./LICENSE) を参照してください
